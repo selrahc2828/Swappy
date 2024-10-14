f62077a8fa2136e0428a8e4313338dafa4e776e5
selrahc2828 <charles.panet@orange.fr>
2024-10-14 21:06:35 +0200

    les modifs que j'ai fais en cours que j'ai oublié de push

--- Assets/Scripts/Physics_Stealable.cs
+++ Assets/Scripts/Physics_Stealable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bouncy : Comportment
+public class Physics_Stealable : Comportment
 {
     public PhysicMaterial bouncyMaterial;
 
