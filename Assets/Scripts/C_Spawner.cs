8e1bdaff9d7ddba1f67f7561340dbe8dc8a2372b
BattleFox200 <marius.macheret@gmail.com>
2024-10-21 14:13:59 +0200

    New Spawner Component

---
+++ Assets/Scripts/C_Spawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C_Spawner : Comportment
+{
+    [SerializeField] private GameObject objectSpawned;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float spawnTime;
+    
+    private float spawnTimer;
+
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        CheckNewComponents();
+
+        if (spawnTimer > spawnTime)
+        {
+            GameObject instantiatedObject = Instantiate(objectSpawned, transform.position, Quaternion.identity);
+            spawnTimer = 0f;
+        }
+    }
+
+    private void CheckNewComponents()
+    {
+        
+    }
+}
