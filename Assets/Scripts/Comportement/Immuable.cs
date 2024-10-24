9da0c3265c7c65b6ee7872ae8f6aae9fc7fdf329
selrahc2828 <charles.panet@orange.fr>
2024-10-24 14:26:50 +0200

    erreur corrigé

--- Assets/Scripts/Comportement/Immuable.cs
+++ Assets/Scripts/Comportement/Immuable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Immuable : Comportement
+public class Immuable : Comportment
 {
     // Start is called before the first frame update
     void Start()
