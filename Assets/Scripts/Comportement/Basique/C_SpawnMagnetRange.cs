84ccaaec0af9a7a55470bd7a26a8be10323df3c1
Marius Macheret <marius.macheret@gmail.com>
2024-12-06 15:10:51 +0100

    Lingering Magnet Bug Fixed + nomenclature

--- Assets/Scripts/Comportement/Basique/C_SpawnMagnetRange.cs
+++ Assets/Scripts/Comportement/Basique/C_SpawnMagnetRange.cs
@@ -7,12 +7,19 @@ public class C_SpawnMagnetRange : Comportment
     public GameObject magnetRange_Prefab;
     public float magnetRange_size;
 
+    private GameObject magnetZone;
+
     void Start()
     {
         if (magnetRange_Prefab)
         {
-            GameObject magnetism = Instantiate(magnetRange_Prefab, transform.position, Quaternion.identity, transform);
-            magnetism.transform.localScale = transform.localScale * magnetRange_size;
+            magnetZone = Instantiate(magnetRange_Prefab, transform.position, Quaternion.identity, transform);
+            magnetZone.transform.localScale = transform.localScale * magnetRange_size;
         }
+    }
+
+    private void OnDisable()
+    {
+        Destroy(magnetZone);
     }
 }
