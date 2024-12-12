c7d36e1fd7103d14ad6f43e65be158868e5ce72f
Entwaane <antoine.vacherie@gmail.com>
2024-12-12 12:08:08 +0100

    Arms_integrés
    pour les mettre dans la scène :
    - asssets > models > Avatar
    - les mettre en enfants de la mainCamera (dans CameraHolder)
    - mettre le Anim_Manager dans la scene (Assets > Prefabs > Animation)
    - renseigner les 2 animators dans l'anim_Manager (Assets > models > Avatar > Animators)

--- Assets/Scripts/Anim/Anim_manager.cs
+++ Assets/Scripts/Anim/Anim_manager.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Anim_manager : MonoBehaviour
 {
+
+    public Animator Left_Arm_Animator;
+    public Animator Right_Arm_Animator;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    #region IDLE 
+    //Pour l'instant inutil mais au cas ou pour plus tard
+    public void Left_Idle()
     {
         
     }
+    public void Right_Idle()
+    {
+        
+    }
+
+    #endregion
+    
+    #region Attribution
+    public void Left_Attribution()
+    {
+        Left_Arm_Animator.SetBool("To_Attrib", true);
+    }
+    public void Right_Attribution()
+    {
+        Right_Arm_Animator.SetBool("To_Attrib", true);
+    }
+    
+    #endregion
+    
+    #region Aspiration
+    public void Left_Aspiration()
+    {
+        Left_Arm_Animator.SetBool("To_Aspir", true);
+    }
+    public void Right_Aspiration()
+    {
+        Right_Arm_Animator.SetBool("To_Aspir", true);
+    }
+    
+    #endregion
 }
