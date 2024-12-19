ff3aeb83ccd93b7e941e38eaddc21d91eb03c2e6
JonathanGautreau <124163841+JonathanGautreau@users.noreply.github.com>
2024-12-19 11:11:22 +0100

    SoundRef mis à jour + comp Place & type fait

--- Assets/Scripts/Sound/Archive/SoundManager.cs
+++ Assets/Scripts/Sound/Archive/SoundManager.cs
@@ -11,41 +11,11 @@ public class SoundManager : MonoBehaviour
 
     #region Init Player
     public enum SoundPlayer { slowTime, unslowTime, steal, give, projectionEnter, projectionStay, projectionExit }
-
-    private RESULT _debug;
-
 #endregion
-    #region Init Comportement
-    public enum SoundCompPlace { repulsePlace, immuablePlace, bouncePlace, propelerPlace, aimantPlace }
-    public enum OnWhatItPlace  { onObject, onPlayer, onGrab }
-    private EventInstance _sonCompPlace;
-    private RESULT _testCompPlace;  
-    private string _repulsePlace = "event:/System/Componenent/Place/repulsePlace";
-    private string _immuablePlace = "event:/System/Componenent/Place/immuablePlace";
-    private string _bouncePlace = "event:/System/Componenent/Place/bouncePlace";
-    private string _propelerPlace = "event:/System/Componenent/Place/propelerPlace";
-    private string _aimantPlace = "event:/System/Componenent/Place/aimantPlace";
+    #region Init System
+    public enum PlaceParamType { repulse, immuable, bounce, propeler, aimant }
+    public enum PlaceParamOnWhatType  { onObject, onPlayer, onGrab }
     public enum SoundComp { repulseBoom,  immuableHit, bounceHit, propelerStart, aimantStart,repulseTimer, }
-    private EventInstance _sonComp;
-    private RESULT _testComp;
-    private string repulseBoom = "event:/System/Componenent/repulseBoom";
-    private string repulseTimer = "event:/System/Componenent/repulseTimer";
-    private string immuableHit = "event:/System/Componenent/immuableHit";
-    private string bounceHit = "event:/System/Componenent/bounceHit";
-    private string propelerStart = "event:/System/Componenent/propelerStart";
-    private string aimantStart = "event:/System/Componenent/aimantStart";
-
-    private EventInstance _sonAimant;
-    private RESULT _testAimant;
-    private EventInstance _sonPropeler;
-    private RESULT _testPropeler;
-    private EventInstance _sonRepulseTimer;
-    private RESULT _testRepulseTimer;
-    #endregion
-    #region Init Collision
-    private EventInstance _sonCollision;
-    private RESULT _testCollisionSound;
-    private string _collision = "event:/System/Collision";
     #endregion
 
     private void Start()
@@ -54,14 +24,9 @@ public class SoundManager : MonoBehaviour
     }
 
     #region Son Player
-
     //                                                                              In Code
     //
-    //
     //  PlayPlayerSound() est à utiliser lorsqu'un des son du joueur doit etre joué.
-    //
-    //  Tout les sons à appelé ici sont des sons finis, donc il ne doivent pas se joué en boucle.
-    //
     //
     //  Sons disponible à ajouter dans le code:
     //  -   slowTime            : PlayPlayerSound(SoundPlayer.slowTime)                 ()
@@ -109,10 +74,7 @@ public class SoundManager : MonoBehaviour
 
     //                                                                              In Animation
     //
-    //
     //  PlaySoundFootstep() est à utiliser dans les animations de marche lorsque le pieds touche le sol.
-    //
-    //  Son finit, donc pas de boucle, en théorie
     //
     //      Annotez l'endroit où footstep player est appelé                             ()
     public void PlaySoundFootstep()
@@ -123,10 +85,8 @@ public class SoundManager : MonoBehaviour
 
     //  PlayPlayerJump() est à utiliser dans les animations de saut lorsque les pieds quitte le sol.
     //
-    //  Son finit, donc pas de boucle, en théorie
-    //
     //      Annotez l'endroit où footstep player est appelé                             ()
-    public void PlayPlayerJump()
+    public void PlaySoundJump()
     {
         RuntimeManager.PlayOneShot(_SoundRef.jump);
     }
@@ -134,10 +94,8 @@ public class SoundManager : MonoBehaviour
 
     //  PlayPlayerLand() est à utiliser dans les animations de saut lorsque les pieds touche le sol.
     //
-    //  Son finit, donc pas de boucle, en théorie
-    //
     //      Annotez l'endroit où footstep player est appelé                             ()
-    public void PlayPlayerLand() 
+    public void PlaySoundLand() 
     {
         RuntimeManager.PlayOneShot(_SoundRef.land);
     }
@@ -146,61 +104,17 @@ public class SoundManager : MonoBehaviour
 
 
     #endregion
-    #region Son Componenet Collectif 
+    #region Son System
+    
     //  PlayComponentPlace() est à utiliser lorsqu'un des comportement est posé sur un objet, le player ou sur un objet grab.
     //  Lors de l'appel de la fonction, vous pouvez ajouter un argement en plus pour choisir l'endroit où est placé le comportement, entre onObject, onPlayer et onGrab (default = onObject)
     //
     //  Tout les sons à appeler ici sont des sons finis, donc il ne doivent pas se joué en boucle.
     //
-    //
-    //  Sons disponible à ajouter dans le code:
-    //  -   repulsePlace        : PlayComponentPlaceSound(SoundCompPlace.repulsePlace)          ()
-    //  -   immuablePlace       : PlayComponentPlaceSound(SoundCompPlace.immuablePlace)         ()
-    //  -   bouncePlace         : PlayComponentPlaceSound(SoundCompPlace.bouncePlace)           ()
-    //  -   propelerPlace       : PlayComponentPlaceSound(SoundCompPlace.propelerPlace)         ()
-    //  -   aimantPlace         : PlayComponentPlaceSound(SoundCompPlace.aimantPlace)           ()
-    //
     //      Annotez quand les sons sont ajouté au code. (nom du script + line)
-    public void PlayComponentPlaceSound(SoundCompPlace soundCompPlace, OnWhatItPlace onWhatItPlace = OnWhatItPlace.onObject)
+    public void PlayComponentPlaceSound(GameObject gameObjet,PlaceParamType soundCompPlace = PlaceParamType.repulse, PlaceParamOnWhatType onWhatItPlace = PlaceParamOnWhatType.onObject)
     {
-        switch(soundCompPlace)
-        {
-            case SoundCompPlace.repulsePlace:
-                _sonCompPlace = RuntimeManager.CreateInstance(_repulsePlace);
-                break;
-            case SoundCompPlace.immuablePlace:
-                _sonCompPlace = RuntimeManager.CreateInstance(_immuablePlace);
-                break;
-            case SoundCompPlace.bouncePlace:
-                _sonCompPlace = RuntimeManager.CreateInstance(_bouncePlace);
-                break;
-            case SoundCompPlace.propelerPlace:
-                _sonCompPlace = RuntimeManager.CreateInstance(_propelerPlace);
-                break;
-            case SoundCompPlace.aimantPlace:
-                _sonCompPlace = RuntimeManager.CreateInstance(_aimantPlace);
-                break;
-            default:
-                UnityEngine.Debug.LogError("PlayPlayerSound, Argument manquant : Checkez la liste");
-                break;
-        }
-        switch(onWhatItPlace) 
-        {
-            case OnWhatItPlace.onObject:
-                _sonCompPlace.setParameterByName("Type", 0);
-                break;
-            case OnWhatItPlace.onPlayer:
-                _sonCompPlace.setParameterByName("Type", 1);
-                break;
-            case OnWhatItPlace.onGrab:
-                _sonCompPlace.setParameterByName("Type", 2);
-                break;
-        }
-        _testCompPlace = _sonCompPlace.start();
-        if (_testCompPlace!= RESULT.OK)
-        {
-            UnityEngine.Debug.LogError("PlayComponentPlaceSound Error: Son non joué/manquant. ( Vous ne devriez au grand jamais voir cette erreur donc chill, mais dans le doute elle est là)");
-        }
+        RuntimeManager.PlayOneShotAttached(_SoundRef.place, gameObject);
     }
 
     //  PlayComponent() est à utiliser lorsqu'un des son de comportement simple doit etre joué.
@@ -214,147 +128,25 @@ public class SoundManager : MonoBehaviour
     //  -   repulseTimer        : PlayComponenentSound(SoundCompPlace.repulseTimer)             ()
     //  -   immuableHit         : PlayComponenentSound(SoundCompPlace.immuableHit)              ()
     //  -   bounceHit           : PlayComponenentSound(SoundCompPlace.bounceHit)                ()
-    //  -   propelerStart       : PlayComponenentSound(SoundCompPlace.propelerStart)            ()
-    //  -   aimantStart         : PlayComponenentSound(SoundCompPlace.aimantStart)              ()
     //
     //      Annotez quand les sons sont ajouté au code. (nom du script + line)
-    public void PlayComponenentSound(SoundComp soundComp, Vector3 positon = default)
+    public void PlayComponenentSound(SoundComp soundComp, GameObject gameObject)
     {
         switch (soundComp)
         {
-            case SoundComp.repulseBoom:
-                _sonComp = RuntimeManager.CreateInstance(repulseBoom);
-                break;
             case SoundComp.repulseTimer:
-                _sonComp = RuntimeManager.CreateInstance(repulseTimer);
+                RuntimeManager.PlayOneShotAttached(_SoundRef.repulseTimer, gameObject);
+                break;
+            case SoundComp.repulseBoom:
+                RuntimeManager.PlayOneShotAttached(_SoundRef.repulseTimer, gameObject);
                 break;
             case SoundComp.immuableHit:
-                _sonComp = RuntimeManager.CreateInstance(immuableHit);
+                RuntimeManager.PlayOneShotAttached(_SoundRef.immuableHit, gameObject);
                 break;
             case SoundComp.bounceHit:
-                _sonComp = RuntimeManager.CreateInstance(bounceHit);
+                RuntimeManager.PlayOneShotAttached(_SoundRef.bounceHit, gameObject);
                 break;
-            //case SoundComp.propelerStart:
-            //    _sonComp = RuntimeManager.CreateInstance(propelerStart);
-            //    break;
-            //case SoundComp.aimantStart:
-            //    _sonComp = RuntimeManager.CreateInstance(aimanatStart);
-            //    break;
-        }
-        _sonComp.set3DAttributes(RuntimeUtils.To3DAttributes(positon));
-        _testComp = _sonComp.start();
-        if (_testComp != RESULT.OK)
-        {
-            UnityEngine.Debug.LogError("PlayComponentSound Error: Son non joué/manquant. ( Vous ne devriez au grand jamais voir cette erreur donc chill, mais dans le doute elle est là)");
         }
     }
     #endregion
-    #region Son Comportement Individuel
-    //  Sons Long/en Loop, dont la position doit etre actualiser à chaque frame.
-    //  Sons en question:
-    //  -   Aimant Start                : PlayAimantSound(transform.position)                   ()
-    //  -   Aimant SetPosition          : SetPositionAimantSound(transform.position)            ()
-    //  -   Aimant Stop                 : StopAimantSound()                                     ()
-    //  -   Propeler Start              : PlayPropelerSound(transform.position)                 ()
-    //  -   Propeler SetPosition        : SetPositionPropelerSound(transform.position)          ()
-    //  -   Propeler Stop               : StopPropelerSound()                                   ()
-    //  -   RepulseTime Start           : PlayRepulseTimeSound(transform.position)              ()
-    //  -   RepulseTime SetPosition     : SetPositionRepulseTimeSound(transform.position)       ()
-    //  -   RepulseTime Stop            : StopRepulseTimeSound()                                ()
-    //
-    //      Annotez quand les sons sont ajouté au code. (nom du script + line)
-    public void PlayAimmantSound(Vector3 position = default)
-    {
-        _sonAimant = RuntimeManager.CreateInstance(aimantStart);
-        _sonAimant.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        _testAimant = _sonAimant.start();
-        if ( _testAimant != RESULT.OK)
-        {
-            UnityEngine.Debug.LogError("PlayAimantSound, Pas de son joué/trouvé, c'est pas bon signe ");
-        }
-    }
-    public void SetPositionAimantSound(Vector3 position = default)
-    {
-        _testAimant = _sonAimant.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        {
-            UnityEngine.Debug.LogError("SetPositionAimantSound, Pas de son joué/trouvé, c'est pas bon signe ");
-        }
-    }
-    public void StopAimantSound()
-    {
-        _sonAimant.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        _sonAimant.release();
-    }
-
-    public void PlayPropelerSound(Vector3 position = default)
-    {
-        _sonPropeler = RuntimeManager.CreateInstance(propelerStart);
-        _sonPropeler.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        _testPropeler = _sonPropeler.start();
-        if (_testPropeler != RESULT.OK)
-        {
-            UnityEngine.Debug.LogError("PlayPropelerSound, Pas de son joué/trouvé, c'est pas bon signe ");
-        }
-    }
-    public void SetPositionPropelerSound(Vector3 position = default)
-    {
-        _testPropeler = _sonPropeler.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        {
-            UnityEngine.Debug.LogError("SetPositionPropelerSound, Pas de son joué/trouvé, c'est pas bon signe ");
-        }
-    }
-    public void StopPropelerSound()
-    {
-        _sonPropeler.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        _sonPropeler.release();
-    }
-
-    public void PlayRepulseTimerSound(Vector3 position = default)
-    {
-        _sonRepulseTimer = RuntimeManager.CreateInstance(propelerStart);
-        _sonRepulseTimer.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        _testRepulseTimer = _sonRepulseTimer.start();
-        if (_testRepulseTimer != RESULT.OK)
-        {
-            UnityEngine.Debug.LogError("PlayRepulseTimerSound, Pas de son joué/trouvé, c'est pas bon signe ");
-        }
-    }
-    public void SetPositionRepulseTimerSound(Vector3 position = default)
-    {
-        _testRepulseTimer = _sonRepulseTimer.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        {
-            UnityEngine.Debug.LogError("SetPositionRepulseTimerSound, Pas de son joué/trouvé, c'est pas bon signe ");
-        }
-    }
-    public void StopRepulseTimerSound()
-    {
-        _sonRepulseTimer.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        _sonRepulseTimer.release();
-    }
-    #endregion
-    #region Son Collison
-    //  Son de collision de tout les objets, à appelé lorsqu'il y a une collision.
-    //  Argument nécessaire :  Position (Vector3) + est-ce que l'objet est magik/à un comportement spéciale (bool)  
-    public void PLayCollisionSound(Vector3 position = default, bool isMagik = false)
-    {
-        _sonCollision = RuntimeManager.CreateInstance(_collision);
-        _sonCollision.set3DAttributes(RuntimeUtils.To3DAttributes(position));
-        if (isMagik)
-        {
-            _sonCollision.setParameterByName("Type", 1);
-        }
-        else
-        {
-            _sonCollision.setParameterByName("Type", 0);
-        }
-        _testCollisionSound = _sonCollision.start();
-        if ( _testCollisionSound != RESULT.OK)
-        {
-            UnityEngine.Debug.LogError("PLayCollisionSound, Pas de son joué/trouvé, des problème de collision");
-        }
-        _sonCollision.release();
-    }
-    #endregion
-
-
 }
--- Assets/Scripts/Sound/Archive/SoundRef.cs
+++ Assets/Scripts/Sound/Archive/SoundRef.cs
@@ -7,16 +7,26 @@ using FMODUnity;
 public class SoundRef : MonoBehaviour
 {
     #region Player
-    public string slowTime {  get; private set; } = "event:/Player/Time/slowTime";
-    public string unslowTime { get; private set; } = "event:/Player/Time/unslowTime";
-    public string steal { get; private set; } = "event:/Player/Comp/stealComp";
-    public string give { get; private set; } = "event:/Player/Comp/giveComp";
+    public string slowTime {  get; private set; } = "event:/Player/Time/SlowTime";
+    public string unslowTime { get; private set; } = "event:/Player/Time/UnslowTime";
+    public string steal { get; private set; } = "event:/Player/Comp/StealComp";
+    public string give { get; private set; } = "event:/Player/Comp/GiveComp";
     public string projectionEnter { get; private set; } = "event:/Player/Projection/projectionEnter";
     public string projectionStay { get; private set; } = "event:/Player/Projection/projectionStay";
     public string projectionExit { get; private set; } = "event:/Player/Projection/projectionExit";
     public string footstep { get; private set; } = "event:/Player/Moving/Footstep";
     public string jump { get; private set; } = "event:/Player/Moving/Jump";
     public string land { get; private set; } = "event:/Player/Moving/Land";
+    #endregion
+    #region System
+    public string collision { get; private set; } = "event:/System/Collision";
+    public string place { get; private set; } = "event:/System/PLace";
+    public string repulseTimer { get; private set; } = "event:/System/Componenent/RepulseTimer";
+    public string repulseBoom { get; private set; } = "event:/System/Componenent/RepulseBoom";
+    public string immuableHit { get; private set; } = "event:/System/Componenent/ImmuableHit";
+    public string bounceHit { get; private set; } = "event:/System/Componenent/ImmuableHit";
+    public string propelerStart { get; private set; } = "event:/System/Componenent/PropelerStart";
+    public string aimantStart { get; private set; } = "event:/System/Componenent/AimantStart";
     #endregion
 
 
