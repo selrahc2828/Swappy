beb5efd984edf1f940a4337f924bbdfb0c01e29f
selrahc2828 <charles.panet@orange.fr>
2025-01-20 11:12:43 +0100

    alerte incendie

--- Assets/Scripts/Test/ControllerPlanete.cs
+++ Assets/Scripts/Test/ControllerPlanete.cs
@@ -9,18 +9,23 @@ public class ControllerPlanete : MonoBehaviour
     public Rigidbody rb;
     public Vector2 moveInputVector;
     public Vector3 moveDirection;
-    protected Controls controls;
+    public Controls controls;
 
     // Start is called before the first frame update
     void Start()
     {
-        controls = GameManager.controls;
+        controls = ouiScript.controls;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        moveInputVector = controls.Player.Movement.ReadValue<Vector2>().normalized;
+        Debug.Log(moveInputVector);
+        //calculate movement direction
+        moveDirection = transform.forward * moveInputVector.y + transform.right * moveInputVector.x;
+        
+        rb.AddForce(moveDirection * 10f, ForceMode.Force);
     }
 }
