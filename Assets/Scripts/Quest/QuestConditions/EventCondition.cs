39aeb40889a3456f5fbba1b260f319fa9715f51a
Marius Macheret <marius.macheret@gmail.com>
2025-04-02 16:10:46 +0200

    Event Condition Working
    Still needs an Overhaul to add feedbacks and make some other random shity events possible (such as onDestroy)

--- Assets/Scripts/Quest/QuestConditions/EventCondition.cs
+++ Assets/Scripts/Quest/QuestConditions/EventCondition.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EventCondition : QuestCondition
 {
+    [Space(16)]
+    [Tooltip("Mettre à 0 pour désactiver le timer et rendre l'EventCondition true de manière permanente")]
+    [SerializeField] private float permissivityTimer;
+    private bool isConditionTrue;
+    private float timer;
 
+    public void SetEventConditionState(bool state)
+    {
+        SetConditionState(state);
+
+        isConditionTrue = state;
+        timer = permissivityTimer;
+    }
+
+    private void Update()
+    {
+        if (permissivityTimer != 0 && isConditionTrue)
+        {
+            timer -= Time.deltaTime;
+            Debug.Log("Timer EventCondition: " + timer);
+
+            if (timer <= 0)
+            {
+                SetConditionState(false);
+
+                isConditionTrue = false;
+            }
+
+        }
+    }
 }
