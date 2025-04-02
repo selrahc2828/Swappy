e6173e0817da082ed3df5c0acda7b644c7a2b209
Marius Macheret <marius.macheret@gmail.com>
2025-04-02 14:22:38 +0200

    QuestCondition Overhaul

--- Assets/Scripts/Quest/QuestCondition.cs
+++ Assets/Scripts/Quest/QuestCondition.cs
@@ -3,5 +3,34 @@ using UnityEngine.Events;
 
 public class QuestCondition : MonoBehaviour
 {
-    public UnityEvent SetConditionBool;
+    [Header("Reference")]
+
+    [Tooltip("La Quête remplie par cette condition")] 
+    [SerializeField] private QuestObject questScript;
+    [Tooltip("En règle générale, laisser l'état de base de la condition à false")] 
+    [SerializeField] private bool defaultState = false;
+    [Tooltip("Activer UNIQUEMENT si ce script est utilisé comme complément d'une autre condition")]
+    [SerializeField] private bool isAdditionalCheck = false;
+
+    private void Start()
+    {
+        if (questScript == null && isAdditionalCheck == false)
+        {
+            Debug.LogError("Il n'y a pas de Gestionnaire de Quête précisé dans cette Condition !");
+            return;
+        }
+
+        questScript.ReferenceThisCondition(this, defaultState);
+    }
+
+    protected void SetConditionState(bool state)
+    {
+        if (isAdditionalCheck)
+        {
+            Debug.LogWarning("Cette condition est marquée comme un AdditionalCheck");
+            return;
+        }
+
+        questScript.ChangeQuestConditions(this, state);
+    }
 }
