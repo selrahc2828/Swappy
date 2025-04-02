eee56f1cdc88e74626e621c80fc6a381b53154e0
Marius Macheret <marius.macheret@gmail.com>
2025-04-03 01:07:16 +0200

    EventCondition Overhaul WIP

--- Assets/Scripts/Quest/QuestConditions/EventCondition.cs
+++ Assets/Scripts/Quest/QuestConditions/EventCondition.cs
@@ -3,9 +3,25 @@ using UnityEngine.EventSystems;
 
 public class EventCondition : QuestCondition
 {
+    private enum ValidationTypes
+    {
+        [Tooltip("Pour lié l'accomplissement quête à une condition d'une autre Quête")]
+        OnQuestFinished,
+        [Tooltip("Lorsque l'objet spécifié est détruit")]
+        OnDestroy,
+        [Tooltip("Autre Events, qui peuvent accéder directement à la fonction SetEventConditonState depuis un autre script")]
+        OtherEvent
+    }
+
+    [Space(16)]
+    [SerializeField] private ValidationTypes validationType;
+    [SerializeField] private Quest questScript;
+    [SerializeField] private GameObject onDestroyedObject;
+
     [Space(16)]
     [Tooltip("Mettre à 0 pour désactiver le timer et rendre l'EventCondition true de manière permanente")]
     [SerializeField] private float permissivityTimer;
+
     private bool isConditionTrue;
     private float timer;
 
