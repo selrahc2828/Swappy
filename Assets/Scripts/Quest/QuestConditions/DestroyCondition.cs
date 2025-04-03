6f7f06d7f5149cdf12bbd2cabbd358488e3b1978
Marius Macheret <marius.macheret@gmail.com>
2025-04-03 10:41:39 +0200

    better variables naming

--- Assets/Scripts/Quest/QuestConditions/DestroyCondition.cs
+++ Assets/Scripts/Quest/QuestConditions/DestroyCondition.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class DestroyCondition : Condition
 {
     [Space(16)]
-    [SerializeField] private bool onDestroyStateChange = true;
+    [SerializeField] private bool onDestroyState = true;
 
     private void OnDestroy()
     {
-        SetConditionState(onDestroyStateChange);
+        SetConditionState(onDestroyState);
     }
 }
