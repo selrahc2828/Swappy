65e140770b8f2acde454b09bb7d7b27ba7660f9d
GregoryGDS <gregory.dossantos98@gmail.com>
2025-04-04 13:42:23 +0200

    Inventory - UI (+ TapeData - EventRef son)

--- Assets/Scripts/ScriptableObjects/TapeData.cs
+++ Assets/Scripts/ScriptableObjects/TapeData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "newTape", menuName = "Scriptable/TapeData", order = 0)]
@@ -8,6 +9,7 @@ public class TapeData : ScriptableObject
     public int itemId;
     public string itemName;
     public Sprite itemSprite;
+    public EventReference musicEvent;
+
     public bool isUnlocked = false;
-    
 }
