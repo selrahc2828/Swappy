dcec94fe6149c3675c3646cfa95fa421e70cbbd6
Marius Macheret <marius.macheret@gmail.com>
2025-04-20 22:36:33 +0200

    BreakableObject Script Add

---
+++ Assets/Scripts/LD/BreakableObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    [SerializeField] private GameObject shatteredVersion;
+    [SerializeField] private float minBreakMagnitude;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+
+        if (collision.relativeVelocity.magnitude > minBreakMagnitude)
+        {
+            GameObject.Instantiate(shatteredVersion, transform.position, Quaternion.identity);
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
