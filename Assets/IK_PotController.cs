1a8672ea335904ea35fefd98d467ee5e8451b01c
GregoryGDS <gregory.dossantos98@gmail.com>
2025-04-22 09:38:17 +0200

    Rename variable + spider exemple

--- Assets/IK_PotController.cs
+++ Assets/IK_PotController.cs
@@ -8,9 +8,9 @@ public class IK_PotController : MonoBehaviour
 {
     [Header("Velocity")] 
     public Transform pot;
-    Vector3 velocity;
-    Vector3 lastVelocity = Vector3.one;
-    private Vector3 lastBodyPosition;
+    Vector3 _velocity;
+    Vector3 _lastVelocity = Vector3.one;
+    private Vector3 _lastBodyPosition;
     public float bodySmoothness = 8; // contrôle du lissage de la velocite 
                                      // plus elle est haute, plus la vitesse change lentement (fluide)
                                      // plus elle est basse, plus la vitesse suit le mouvement du corps
@@ -19,18 +19,19 @@ public class IK_PotController : MonoBehaviour
     
     [Header("References")]
     public Transform[] legTargets; // liste des jambes à bouger
-    [FormerlySerializedAs("defaultLegTransforms")] public Transform[] legCubesDefault;// liste des positions par défaut
+    public Transform[] legCubesDefault;// liste des positions par défaut
     
-    public Vector3[] legPositions; //position jambe
-    [FormerlySerializedAs("lastLegPosition")] public Vector3[] legOriginalPositions; // dernière position / position d'origine
+    private Vector3[] _legPositions; //position jambe
+    private Vector3[] _legOriginalPositions; // dernière position / position d'origine
     
     [Header("Step Parameters")]
     public float areaOffset = 1.5f; // zone dans laquelle la jambe doit rester
                                     // si elle est plus loin, la jambe est replacé
-    [FormerlySerializedAs("stepOffset")] public float stepHeight = .5f; // hauteur de pas
+    public float stepHeight = .5f; // hauteur de pas
     public float localPlacement = .3f; // ecart entre corps et jambe
     public AnimationCurve stepCurve;
     public float stepDuration;
+    public float waitTimeBetweenTwoSteps = 1f;
     
     public bool[] legMoving;
 
@@ -41,8 +42,8 @@ public class IK_PotController : MonoBehaviour
     void Start()
     {
         nbLeg = legTargets.Length;
-        legPositions = new Vector3[nbLeg];
-        legOriginalPositions = new Vector3[nbLeg];
+        _legPositions = new Vector3[nbLeg];
+        _legOriginalPositions = new Vector3[nbLeg];
 
         legMoving = new bool[nbLeg];
         
@@ -56,71 +57,50 @@ public class IK_PotController : MonoBehaviour
             Vector3 position = legTargets[i].position;
 
             
-            legPositions[i] = position;
-            legOriginalPositions[i] = position;
+            _legPositions[i] = position;
+            _legOriginalPositions[i] = position;
             
             legMoving[i] = false;
         }
         
-        lastBodyPosition = pot.transform.position;
+        _lastBodyPosition = pot.transform.position;
     }
 
     void FixedUpdate()
     {
-        velocity = pot.transform.position - lastBodyPosition;
-        velocity = (velocity + bodySmoothness * lastVelocity) / (bodySmoothness + 1f);
-        Debug.DrawRay(pot.transform.position, velocity * 10, Color.red);
-
+        _velocity = pot.transform.position - _lastBodyPosition;
+        _velocity = (_velocity + bodySmoothness * _lastVelocity) / (bodySmoothness + 1f);
+        Debug.DrawRay(pot.transform.position, _velocity * 15, Color.blue);
         
-        // si on utilise pas la physique pour bouger le corps : 
-        // bodyVelocity = (transform.position - lastBodyPosition) / Time.fixedDeltaTime;
-        // lastBodyPosition = transform.position;
         
         for (int i = 0; i < nbLeg; i++)
         {
             // position ideal dans le monde
             Vector3 worldIdealPos = legCubesDefault[i].position; //.parent.TransformPoint(legCubesDefault[i].localPosition)
             
-            
-            // on doit definir une position "futur" de prediction, sinon la jambe restera en arriere lorsque le corps avance
-            // si on prend juste la position par defaut voulu, le coprs continue d'avancer, résultat la jambe est en retard
-
-            float distance = Vector3.Distance(legOriginalPositions[i], worldIdealPos);
+            float distance = Vector3.Distance(_legOriginalPositions[i], worldIdealPos);
             
             // si jambe est trop loin de sa position ideal, on la déplace
             if (distance >= areaOffset && !legMoving[i])
             {
-               
-
-
-                
-                //     // Vector3 targetPost = defaultLegTransforms[i].position;
-                //     //legTargets[i].position = defaultLegTransforms[i].position;
-
+                // on doit definir une position "future" de prediction, sinon la jambe restera en arriere lorsque le corps avance
+                // si on prend juste la position par defaut voulu, le coprs continue d'avancer, resultat la jambe est en retard
                 Vector3 targetPosition = 
                     legCubesDefault[i].position
-                    + Mathf.Clamp(velocity.magnitude * velocityMultiplier, 0.0f, 1.5f) 
+                    + Mathf.Clamp(_velocity.magnitude * velocityMultiplier, 0.0f, 1.5f) 
                     * (legCubesDefault[i].position - legTargets[i].transform.position) 
-                    + velocity * velocityMultiplier;
-                Debug.Log($"default: {legCubesDefault[i].position}\n" +
-                          $"recalc: {targetPosition}");
+                    + _velocity * velocityMultiplier;
+                
                 StartCoroutine(MoveLeg(i, targetPosition));
-                
-                Debug.DrawLine(legTargets[i].position, targetPosition, Color.green);
-                Debug.DrawRay(legTargets[i].position, velocity * 10, Color.red);
-                
             }
             else
             {
-                legTargets[i].position = legOriginalPositions[i];
+                legTargets[i].position = _legOriginalPositions[i];
             }
-
-            //legTargets[i].position = lastLegPosition[i];
         }
         
-        lastBodyPosition = pot.transform.position;
-        lastVelocity = velocity;
-        
+        _lastBodyPosition = pot.transform.position;
+        _lastVelocity = _velocity;
     }
 
     IEnumerator  MoveLeg(int legIndex, Vector3 targetPosition)
@@ -128,7 +108,7 @@ public class IK_PotController : MonoBehaviour
         legMoving[legIndex] = true;
 
         float t = 0f;
-        Vector3 start = legOriginalPositions[legIndex];
+        Vector3 start = _legOriginalPositions[legIndex];
 
         while (t < 1f)
         {
@@ -144,9 +124,12 @@ public class IK_PotController : MonoBehaviour
         
         Debug.Log($"after move {legIndex}");
         legTargets[legIndex].position = targetPosition;
-        legPositions[legIndex] = targetPosition;
-        legOriginalPositions[legIndex] = targetPosition;
+        _legPositions[legIndex] = targetPosition;
+        _legOriginalPositions[legIndex] = targetPosition;
 
+        for (int i = 1; i <= waitTimeBetweenTwoSteps; ++i) yield return new WaitForFixedUpdate();
+        //temps d'attente avant que la jambe refasse un pas
+        
         legMoving[legIndex] = false;
     }
     
@@ -159,19 +142,16 @@ public class IK_PotController : MonoBehaviour
         //Gizmos.color = Color.green;
         foreach (Transform pos in legCubesDefault)
         {
-            // Gizmos.DrawWireSphere(pos.position, areaOffset);
             Gizmos.DrawWireSphere(pos.position, areaOffset);
         }
         Gizmos.color = Color.yellow;
         foreach (Transform pos in legTargets)
         {
-            // Gizmos.DrawWireSphere(pos.position, areaOffset);
             Gizmos.DrawWireSphere(pos.position, areaOffset);
         }
         Gizmos.color = Color.blue;
-        foreach (Vector3 pos in legOriginalPositions)
+        foreach (Vector3 pos in _legOriginalPositions)
         {
-            // Gizmos.DrawWireSphere(pos.position, areaOffset);
             Gizmos.DrawSphere(pos, .1f);
         }
     }
