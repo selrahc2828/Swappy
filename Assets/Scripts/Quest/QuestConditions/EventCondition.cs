0e11c649f2df172b632e10ba9b15e7d1826c5f75
Marius Macheret <marius.macheret@gmail.com>
2025-04-24 13:31:21 +0200

    EventCondition Correct

--- Assets/Scripts/Quest/QuestConditions/EventCondition.cs
+++ Assets/Scripts/Quest/QuestConditions/EventCondition.cs
@@ -1,43 +1,19 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class EventCondition : Condition
 {
     [Space(16)]
-    [Tooltip("Mettre à 0 pour désactiver le timer et rendre l'EventCondition true de manière permanente")]
-    [SerializeField] private float permissivityTimer;
     [Tooltip("Uniquement utile pour donner une destination aux lignes de quêtes debug")]
     [SerializeField] private GameObject eventSource;
 
-    private bool isConditionTrue;
-    private float timer;
-
     public void SetEventConditionState(bool state)
     {
-        attachedQuest.SetCondition(this,state);
-
-        isConditionTrue = state;
-        timer = permissivityTimer;
+        SetConditionState(state);
     }
 
-    private void Update()
-    {
-        if (permissivityTimer != 0 && isConditionTrue)
-        {
-            timer -= Time.deltaTime;
-            Debug.Log("Timer EventCondition: " + timer);
-
-            if (timer <= 0)
-            {
-                attachedQuest.SetCondition(this, false); ;
-
-                isConditionTrue = false;
-            }
-
-        }
-    }
     protected override Vector3 GetConditionLineStart()
     {
+       
         return eventSource.transform.position;
     }
 }
