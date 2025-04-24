a9fb6c5b4e034dd616e285d5c772db69a909cbbc
Marius Macheret <marius.macheret@gmail.com>
2025-04-25 00:06:19 +0200

    Add StartState Operator

--- Assets/Scripts/Quest/Operators/StartStateOperator.cs
+++ Assets/Scripts/Quest/Operators/StartStateOperator.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class StartStateOperator : OperatorCondition
 {
+    [SerializeField] private bool startState;
 
+    private void Start()
+    {
+        ConditionOutput.Invoke(startState, this);
+    }
+
+    public override void OperatorInput(bool state, Condition source)
+    {
+        SetConditionState(state);
+    }
 }
