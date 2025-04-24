fd2d77af5cb778f3d4b811f47b1be46f85009801
Marius Macheret <marius.macheret@gmail.com>
2025-04-24 15:03:02 +0200

    AndOrOperator Add

--- Assets/Scripts/Quest/Operators/AndOrOperator.cs
+++ Assets/Scripts/Quest/Operators/AndOrOperator.cs
@@ -1,6 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AndOrOperator : OperatorCondition
 {
+    public enum OperatorTypes
+    {
+        And,
+        Or
+    }
 
+    [SerializeField] private OperatorTypes operatorType;
+    [SerializeField] private int argumentCount;
+
+    private Dictionary<Condition, bool> ActiveConditions = new Dictionary<Condition, bool>();
+
+    public override bool OperatorInput(bool state, Condition source)
+    {
+        switch (operatorType)
+        {
+            case OperatorTypes.And:
+                if (argumentCount != ActiveConditions.Count)
+                {
+                    return false;
+                }
+                foreach (KeyValuePair<Condition, bool> conditionRef in ActiveConditions)
+                {
+                    if (conditionRef.Value == false)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+
+            case OperatorTypes.Or:
+                foreach (KeyValuePair<Condition, bool> conditionRef in ActiveConditions)
+                {
+                    if (conditionRef.Value == true)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+                default: 
+                    return false;
+        }
+    }
 }
