755780449151182c7d5de90374a78869e392b994
selrahc2828 <charles.panet@orange.fr>
2025-05-01 17:14:12 +0200

    minimap

--- Assets/Scripts/Feedback/MiniMap/LocalisationGPS.cs
+++ Assets/Scripts/Feedback/MiniMap/LocalisationGPS.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class LocalisationGPS : MonoBehaviour
 {
     public GameObject miniMapPlayer;
+    public GameObject miniMapPlanet;
     public GameObject miniMapMainPlanetCore;
     public GameObject mainPlanetCore;
     
-    [Range(0.0001f, 0.001f)]
+    [Range(0.0001f, 1f)]
     public float scaleRatio = 5f;
 
     // Update is called once per frame
@@ -17,5 +18,10 @@ public class LocalisationGPS : MonoBehaviour
         Vector3 relativeLocalPos = mainPlanetCore.transform.InverseTransformPoint(GameManager.Instance.player.transform.position);
         Vector3 scaledLocalPos = relativeLocalPos * scaleRatio;
         miniMapPlayer.transform.position = miniMapMainPlanetCore.transform.TransformPoint(scaledLocalPos);
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            miniMapPlanet.SetActive(!miniMapPlanet.activeInHierarchy);
+            miniMapPlayer.SetActive(!miniMapPlayer.activeInHierarchy);
+        }
     }   
 }
