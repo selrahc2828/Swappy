3b9820ffc7d91a23153adf71d0adb32ca7b27182
Marius Macheret <marius.macheret@gmail.com>
2025-05-17 13:09:10 +0200

    Add Script Separation

---
+++ Assets/Scripts/Player/Camera/PlayerCamPolish.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+    public class PlayerCamPolish : MonoBehaviour
+    {
+        [Header("Jump Curves")]
+        [SerializeField] private float camOnJump;
+        [SerializeField] private float camOnJumpTime;
+        [SerializeField] private AnimationCurve camOnJumpCurve;
+        [Space(8)]
+        [SerializeField] private float camOffJumpTime;
+        [SerializeField] private AnimationCurve camOffJumpCurve;
+
+        [Header ("Arm Curves")]
+        [SerializeField] private float armRailRatio;
+        [SerializeField] private float armRailMaxOffset;
+        [SerializeField] private AnimationCurve armRailCurve;
+
+        [Header ("Collision Curves")]
+        [SerializeField] private float camOnGround;
+        [SerializeField] private float camOnGroundTime;
+        [SerializeField] private float camOnGroundMax;
+        [SerializeField] private float camOnGroundMin;
+        [SerializeField] private float camOnGroundForceRatio;
+        [SerializeField] private AnimationCurve camOnGroundCurve;
+        [Space(8)]
+        [SerializeField] private float camOnWall;
+        [SerializeField] private float camOnWallTime;
+        [SerializeField] private AnimationCurve camOnWallCurve;
+
+
+        private Vector3 mergedCamOffset;
+    }
---
+++ Assets/Scripts/Player/Controller/PlayerPhysicsPolish.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+    public class PlayerPhysicsPolish : MonoBehaviour
+    {
+        [Header("Physics Curves")]
+        private AnimationCurve walkSpeedOnMoveCurve;
+        private AnimationCurve walkSpeedOffMoveCurve;
+
+        private float maxSpeedOnJump;
+        private AnimationCurve maxSpeedOnJumpCurve;
+        private float maxSpeedOffJumpTime;
+        private AnimationCurve maxSpeedOffJumpCurve;
+
+        private float airControlMinimum;
+        private AnimationCurve airControlCurve;
+    }
