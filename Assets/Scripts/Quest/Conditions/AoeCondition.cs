b8d4eee49a79a7a07b7f6d5b53fa0f94bb335996
Marius Macheret <124874571+BattleFox200@users.noreply.github.com>
2025-05-27 12:36:31 +0200

    AoeConditionAdd

---
+++ Assets/Scripts/Quest/Conditions/AoeCondition.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using UnityEngine;
+
+public class AoeCondition: AdditionalCondition
+{
+    private enum CompCategory
+    {
+        Repulse,
+        ForceRepulse,
+        Magnet,
+        MagnetRepulse
+    }
+
+    [Tooltip("Si targetObject vide, le script checkera à chaque vol / réattribution de comportement")]
+    public GameObject targetObject;
+    [SerializeField] private CompCategory AoeType;
+
+    [Space(8)]
+    [SerializeField] private FirstState targetComportment;
+    [SerializeField] private CompCategory targetCompCategory;
+
+    private void Update()
+    {
+        //A remplacer par une activation lorsque le joueur input un vol / réattribution de comportement
+        if (targetObject != null)
+        {
+            SetConditionState(CheckObjectParameters(targetObject));
+        }
+    }
+
+    public override bool CheckObjectParameters(GameObject target)
+    {
+        if (target.GetComponent<ComportementsStateMachine>() == null)
+        {
+            return false;
+        }
+
+        ComportementState compState = (ComportementState)target.GetComponent<ComportementsStateMachine>().currentState;
+      
+        switch (AoeType)
+        {
+            case ValidationTypes.Comportment:
+
+                if (compState.stateValue == (int)targetComportment)
+                {
+                    return(true);
+                }
+                return (false);
+
+            case ValidationTypes.CompCategory:
+
+                int leftComp = compState.leftValue;
+                int rightComp = compState.rightValue;
+
+                switch (targetCompCategory)
+                {
+                    case CompCategory.None:
+                        if (rightComp == 0 && leftComp == 0)
+                        {
+                            return (true);
+                        }
+                        break;
+
+                    case CompCategory.Solo:
+                        if (rightComp == 0 && leftComp != 0)
+                        {
+                            return (true);
+                        }
+                        break;
+
+                    case CompCategory.Duo:
+                        if (rightComp != 0 && rightComp == leftComp)
+                        {
+                            return (true);
+                        }
+                        break;
+
+                    case CompCategory.Fusion:
+                        if (rightComp != 0)
+                        {
+                            return (true);
+                        }
+                        break;
+
+                    case CompCategory.StrictFusion:
+                        if (rightComp != 0 && rightComp != leftComp)
+                        {
+                            return (true);
+                        }
+                        break;
+                
+                    default:
+                        return(false);
+
+                }
+                break;
+            default:
+                return (false);
+        }
+        return(false);
+    }
+}
